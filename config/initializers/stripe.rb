2287b990a08d3f6b5019e62530ca59b5375a494a
omar6685 <omr6685@hotmail.com>
2022-09-21 22:45:36 +0300

    test

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,8 @@
-if Rails.application.credentials[:stripe].present? && Rails.application.credentials[:stripe][:secret].present?
-  Stripe.api_key = Rails.application.credentials[:stripe][:secret]
+module Phonei
+  class Application < Rails::Application
+    config.after_initialize do
+      # initialization code goes here
+      Stripe.api_key = Rails.application.credentials[:stripe][:secret]
+    end
+  end
 end
