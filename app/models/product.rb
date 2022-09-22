3b8f97505d491fcb20ae43b76e96eb0fd0042461
omar6685 <omr6685@hotmail.com>
2022-09-22 10:47:59 +0300

    editing models

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,20 +7,12 @@ class Product < ApplicationRecord
     name
   end
 
-  monetize :price, as: :price_cents
 
-  # transform shopping cart products into an array of line items
-  def to_builder
-    Jbuilder.new do |product|
-      product.price stripe_price_id
-      product.quantity 1
-    end
-  end
-
+  
   # create stripe product and assign to this product
   after_create do
     product = Stripe::Product.create(name: name)
-    price = Stripe::Price.create(product: product, unit_amount: self.price, currency: self.currency)
+    price = Stripe::Price.create(product: product, unit_amount: self.price, currency: "usd")
     update(stripe_product_id: product.id, stripe_price_id: price.id)
   end
 
@@ -29,12 +21,12 @@ class Product < ApplicationRecord
   # this is all additional complexity.
   # better just create new products.
 
-  # after_update :create_and_assign_new_stripe_price, if: :saved_change_to_price?
+  after_update :create_and_assign_new_stripe_price, if: :saved_change_to_price?
   # after_update :create_and_assign_new_stripe_price, if: :saved_change_to_currency?
 
-  # def create_and_assign_new_stripe_price
-  #   price = Stripe::Price.create(product: self.stripe_product_id, unit_amount: self.price, currency: self.currency)
-  #   update(stripe_price_id: price.id)
-  # end
+  def create_and_assign_new_stripe_price
+    price = Stripe::Price.create(product: self.stripe_product_id, unit_amount: self.price, currency: self.currency)
+    update(stripe_price_id: price.id)
+  end
 
 end
