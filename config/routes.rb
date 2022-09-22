41d7ac6ff2408a614c0cf703fae752e81c23983b
omar6685 <omr6685@hotmail.com>
2022-09-22 13:36:07 +0300

    editing show and delete bottun

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,5 @@ Rails.application.routes.draw do
   get "success", to: "checkout#success"
   get "cancel", to: "checkout#cancel"
 
-  post "products/add_to_cart/:id", to: "products#add_to_cart", as: "add_to_cart"
-  delete "products/remove_from_cart/:id", to: "products#remove_from_cart", as: "remove_from_cart"
 
 end
