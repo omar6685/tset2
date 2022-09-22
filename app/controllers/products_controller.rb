9aea8ceb03b719c661fe6fc3cf85c695b939fdcc
omar6685 <omr6685@hotmail.com>
2022-09-22 14:47:02 +0300

    finshing with images

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -40,6 +40,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:name, :price)
+      params.require(:product).permit(:name, :price, :brand, :model, :description, :condition, :color,:image)
     end
 end
