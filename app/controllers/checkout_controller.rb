0ba96a845197dc47ffd048d1052085daa01087b9
omar6685 <omr6685@hotmail.com>
2022-09-22 10:34:37 +0300

    editing controllers

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,32 +1,27 @@
 class CheckoutController < ApplicationController
   
   def create
-    if @cart.pluck(:currency).uniq.length > 1
-      redirect_to products_path, alert: "You can not select products with different currencies in one checkout"
-    else
-      @session = Stripe::Checkout::Session.create({
-        customer: current_user.stripe_customer_id,
-        payment_method_types: ['card'],
-        line_items: @cart.collect { |item| item.to_builder.attributes! },
-        allow_promotion_codes: true,
-        mode: 'payment',
-        success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
-        cancel_url: cancel_url,
+    product = Product.find(params[:id])
+    @session = Stripe::Checkout::Session.create({
+      customer: current_user.stripe_customer_id,
+      payment_method_types: ['card'],
+      line_items: [{
+         price: product.stripe_price_id,
+         quantity: 1
+      }],
+      allow_promotion_codes: true,
+      mode: 'payment',
+       success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
+       cancel_url: cancel_url,
       })
-      redirect_to @session.url
-    end
+    redirect_to @session.url
+    
   end
 
   def success
-    if params[:session_id].present? 
-      # session.delete(:cart)
-      session[:cart] = [] # empty cart = empty array
-      @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ["line_items"]})
-      @session_with_expand.line_items.data.each do |line_item|
-        product = Product.find_by(stripe_product_id: line_item.price.product)
-      end
-    else
-      redirect_to cancel_url, alert: "No info to display"
+    @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ["line_items"]})
+    @session_with_expand.line_items.data.each do |line_item|
+      product = Product.find_by(stripe_product_id: line_item.price.product)
     end
   end
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show update destroy ]
 
-  def add_to_cart
-    id = params[:id].to_i
-    session[:cart] << id unless session[:cart].include?(id)
-    redirect_to products_path
-  end
-
-  def remove_from_cart
-    id = params[:id].to_i
-    session[:cart].delete(id)
-    redirect_to products_path
-  end
-
   def index
     @products = Product.all
   end
@@ -52,6 +40,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:name, :price, :price_cents, :currency)
+      params.require(:product).permit(:name, :price)
     end
 end
