fd19956707c3ab8d78838415c4d73f7fa72b3438
omar6685 <omr6685@hotmail.com>
2022-09-23 10:45:16 +0300

    editing webhook

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,6 @@
 require 'stripe'
 
 Stripe.api_key = 'sk_test_51LZDGTCd3RiW6igw7nTD6ACu87FKxGZhYlFmxGmp4ZN4LXggnpfdESHCTyjyvB0eLz5pJ6kNVa0hLR1Ng9jTHdse00WI2hbzaC'
+endpoint_secret = 'whsec_49bcygiLavzBn6yvUKLSMY4XN0onuKcy'
+
 
