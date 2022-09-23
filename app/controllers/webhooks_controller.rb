ffa9a4ae439a317cab86bc22e7798d0503baa5e1
omar6685 <omr6685@hotmail.com>
2022-09-23 12:56:48 +0300

    editing webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,9 +3,6 @@ class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    
-    endpoint_secret = 'whsec_49bcygiLavzBn6yvUKLSMY4XN0onuKcy'
-    set :port, 4242
     payload = request.body.read
     sig_header = request.env['HTTP_STRIPE_SIGNATURE']
     event = nil
