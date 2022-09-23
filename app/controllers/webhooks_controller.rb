10c1510e68ec0e64a8a8c0b0b6a99a7f7dec1a32
omar6685 <omr6685@hotmail.com>
2022-09-23 13:02:25 +0300

    editing webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,35 +3,33 @@ class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    payload = request.body.read
-    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
-    event = nil
-
-    begin
-      event = Stripe::Webhook.construct_event(
-        payload, sig_header, endpoint_secret
-      )
-    rescue JSON::ParserError => e
-      status 400
-      return
-    rescue Stripe::SignatureVerificationError => e
-      # Invalid signature
-      puts "Signature error"
-      p e
-      return
-    end
-
-    # Handle the event
-    case event.type
-    when 'checkout.session.completed'
-      session = event.data.object
-      session_with_expand = Stripe::Checkout::Session.retrieve({ id: session.id, expand: ["line_items"]})
-      session_with_expand.line_items.data.each do |line_item|
-        product = Product.find_by(stripe_product_id: line_item.price.product)
-        product.increment!(:sales_count)
-      end
-    end
-
-    render json: { message: 'success' }
+    endpoint_secret = 'whsec_49bcygiLavzBn6yvUKLSMY4XN0onuKcy'
+    
+    set :port, 4242
+    
+    post '/webhook' do
+        payload = request.body.read
+        sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+        event = nil
+    
+        begin
+            event = Stripe::Webhook.construct_event(
+                payload, sig_header, endpoint_secret
+            )
+        rescue JSON::ParserError => e
+            # Invalid payload
+            status 400
+            return
+        rescue Stripe::SignatureVerificationError => e
+            # Invalid signature
+            status 400
+            return
+        end
+    
+        # Handle the event
+        puts "Unhandled event type: #{event.type}"
+    
+        status 200
+    end}
   end
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,53 +2,8 @@
 require 'json'
 require 'sinatra'
 require 'stripe'
-
+    
 Stripe.api_key = 'sk_test_51LZDGTCd3RiW6igw7nTD6ACu87FKxGZhYlFmxGmp4ZN4LXggnpfdESHCTyjyvB0eLz5pJ6kNVa0hLR1Ng9jTHdse00WI2hbzaC'
-
-
-# server.rb
-#
-# Use this sample code to handle webhook events in your integration.
-#
-# 1) Paste this code into a new file (server.rb)
-#
-# 2) Install dependencies
-#   gem install sinatra
-#   gem install stripe
-#
-# 3) Run the server on http://localhost:4242
-#   ruby server.rb
-
-require 'json'
-require 'sinatra'
-require 'stripe'
-
-# This is your Stripe CLI webhook secret for testing your endpoint locally.
 endpoint_secret = 'whsec_49bcygiLavzBn6yvUKLSMY4XN0onuKcy'
 
-set :port, 4242
 
-post '/webhook' do
-    payload = request.body.read
-    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
-    event = nil
-
-    begin
-        event = Stripe::Webhook.construct_event(
-            payload, sig_header, endpoint_secret
-        )
-    rescue JSON::ParserError => e
-        # Invalid payload
-        status 400
-        return
-    rescue Stripe::SignatureVerificationError => e
-        # Invalid signature
-        status 400
-        return
-    end
-
-    # Handle the event
-    puts "Unhandled event type: #{event.type}"
-
-    status 200
-end
